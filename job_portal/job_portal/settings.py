2704b68513ccd801f421d56b997c739f2506a4b7
abrs7 <abrishsambong@gmail.com>
2024-04-01 21:35:20 +0300

    Profile Section Created

--- job_portal/job_portal/settings.py
+++ job_portal/job_portal/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'accounts',
     'resume',
     
+    'profiles',
+    
 ]
 
 MIDDLEWARE = [
---
+++ job_portal/profiles/models.py
@@ -0,0 +1,34 @@
+from typing import Any
+from django.db import models
+from django.core.validators import MaxValueValidator, FileExtensionValidator
+from accounts.models import User
+from .validators import size_validator
+
+# Create your models here.
+
+class CompanyProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length = 100)
+    logo = models.ImageField(upload_to='company_logo', validators=[FileExtensionValidator(['pdf', 'png']), size_validator])
+    established_date = models.CharField(max_length = 100 )
+    address = models.CharField(max_length = 300 )
+    license_image = models.ImageField(upload_to='company_license', validators=[FileExtensionValidator(['pdf', 'png']), size_validator])
+    company_email = models.EmailField(max_length =100)
+    company_website = models.URLField(max_length= 100, required=False )
+    company_detail = models.TextField(max_length= 10000, required=False)
+
+    def __init__(self):
+        return f"user: {self.user}, name: {self.name}"
+    
+class ContractorProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length = 100)
+    last_name = models.CharField(max_length = 100)
+    profile_pic = models.ImageField(upload_to='company_logo', validators=[FileExtensionValidator(['pdf', 'png']), size_validator])
+    address = models.CharField(max_length = 300 )
+    personal_email = models.EmailField(max_length =100)
+    
+
+    def __init__(self):
+        return f"user: {self.user}, name: {self.first_name}"    
+
